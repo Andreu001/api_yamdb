cd3abc4d8e7e6122b865342a13dcedaa458bb9d2
Anastasiya <anastasiyaalexxx@gmail.com>
2023-02-15 19:57:11 +0300

    внесены модели и view

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.viewsets import ModelViewSet
+from django.db.models import Avg
+from reviews.models import Category, Genre, Title
+from api.filters import TitleFilter
 
-# Create your views here.
+from .mixins import ModelMixinSet
+from .permissions import (IsAdminUserOrReadOnly)
+from .serializers import (CategorySerializer,
+                          GenreSerializer,
+                          TitleReadSerializer,
+                          TitleWriteSerializer)
+
+
+class CategoryViewSet(ModelMixinSet):
+    """
+    Получить список всех категорий. Права доступа: Доступно без токена
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (IsAdminUserOrReadOnly,)
+    filter_backends = (SearchFilter, )
+    search_fields = ('name', )
+    lookup_field = 'slug'
+
+
+class GenreViewSet(ModelMixinSet):
+    """
+    Получить список всех жанров. Права доступа: Доступно без токена
+    """
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (IsAdminUserOrReadOnly,)
+    filter_backends = (SearchFilter,)
+    search_fields = ('name', )
+    lookup_field = 'slug'
+
+
+class TitleViewSet(ModelViewSet):
+    """
+    Получить список всех объектов. Права доступа: Доступно без токена
+    """
+    queryset = Title.objects.annotate(
+        rating=Avg('reviews__score')
+    ).all()
+    permission_classes = (IsAdminUserOrReadOnly,)
+    filter_backends = (DjangoFilterBackend, )
+    filterset_class = TitleFilter
+
+    def get_serializer_class(self):
+        if self.action in ('list', 'retrieve'):
+            return TitleReadSerializer
+        return TitleWriteSerializer
